aa513369ecd440dd8131beacc026f06e359391de
nitinkumpal <nitinkumpal@gmail.com>
2022-07-09 22:54:09 +0530

    1st commit to Demo project

---
+++ Demo/myfirst-app/src/first.js
@@ -0,0 +1,16 @@
+//this imports React and Component
+import React,{Component} from "react";
+
+//this create the class and converts into component
+class First extends React.Component{
+    render(){ //acts like a main() in java
+        return(
+            <div>
+                <h1>First React Application</h1>
+            </div>
+        )
+
+    }
+
+}
+export default First;
